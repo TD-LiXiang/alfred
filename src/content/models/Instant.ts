7e35303f382c1e4b93ad7677d08f7406c263cd32
TD-XiPengZhao <xipeng.zhao@china.talkdesk.com>
2022-06-17 16:17:55 +0800

    feat: add instants search

---
+++ src/content/models/Instant.ts
@@ -0,0 +1,12 @@
+interface Instant{
+    date:string,
+    help_center:string,
+    multibrand:boolean,
+    parent_title:string,
+    section_or_topic:string,
+    title:string,
+    type:string,
+    url:string
+}
+
+export default Instant
---
+++ src/content/models/InstantResponse.ts
@@ -0,0 +1,7 @@
+import Instant from "./Instant"
+
+interface InstantResponse{
+    results:Instant[]
+}
+
+export default InstantResponse
